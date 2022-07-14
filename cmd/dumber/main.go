8a517f5261a9fd878c839200152be3f0ff802670
Michel Boucey <michel.boucey@gmail.com>
2022-07-14 14:05:08 +0200

    Temporary file has now a random name

--- cmd/dumber/main.go
+++ cmd/dumber/main.go
@@ -1,3 +1,23 @@
+// dumber, a command line tool for numbering Mardown sections
+//
+// Copyright (c) 2021-2022 Michel Boucey
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without
+//    specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+// OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 package main
 
 import (
@@ -15,15 +35,14 @@ import (
 
 func main() {
 
-	version := "1.1.0"
+	version := "1.1.1"
 
-	var section string
 	var headerCounters [7]int
-	var rewrittenLine string
-	var tmpFilePath string
 	var mdTmpFile *os.File
-	var pathSep string
 	var newLine string
+	var pathSep string
+	var rewrittenLine string
+	var section string
 
 	switch runtime.GOOS {
 	case "windows":
@@ -34,15 +53,15 @@ func main() {
 		newLine = "\n"
 	}
 
-	versionFlag := flag.Bool("v", false, "Show version")
 	helpFlag := flag.Bool("h", false, "Show help")
-	writeFlag := flag.Bool("w", false, "Write section numbers to the .md file (default to stdout)")
 	removeFlag := flag.Bool("r", false, "Remove section numbers from the .md file")
+	versionFlag := flag.Bool("v", false, "Show version")
+	writeFlag := flag.Bool("w", false, "Write section numbers to the .md file (default to stdout)")
 
 	flag.Parse()
 
 	if *versionFlag == true {
-		fmt.Println("dumber v" + version + newLine + "Copyright © 2021 Michel Boucey" + newLine + "Released under 3-Clause BSD License")
+		fmt.Println("dumber v" + version + newLine + "Copyright © 2021-2022 Michel Boucey" + newLine + "Released under 3-Clause BSD License")
 		os.Exit(-1)
 	}
 
@@ -69,10 +88,9 @@ func main() {
 	}
 
 	if *writeFlag {
-		tmpFilePath = filepath.Dir(mdFilePath) + pathSep + "." + filepath.Base(mdFilePath) + ".tmp"
-		mdTmpFile, err = os.Create(tmpFilePath)
+		mdTmpFile, err = os.CreateTemp(filepath.Dir(mdFilePath)+pathSep, ".dumber-*.tmp")
 		if err != nil {
-			log.Println(err)
+			log.Fatal(err)
 		}
 	}
 
@@ -138,7 +156,7 @@ func main() {
 	if *writeFlag {
 		mdTmpFile.Close()
 
-		err = os.Rename(tmpFilePath, mdFilePath)
+		err = os.Rename(mdTmpFile.Name(), mdFilePath)
 		if err != nil {
 			log.Fatal(err)
 		}
